88605b7f537bf82025cb6dc824ec6892101cc805
bugsiesegal <bugsie@segalnyc.com>
2022-08-01 09:19:44 -0400

    Changed Model and changed train

--- BrainwaveTools/ai_model.py
+++ BrainwaveTools/ai_model.py
@@ -6,148 +6,165 @@ from keras.layers.reshaping import Reshape
 from keras.layers.rnn import TimeDistributed
 from keras.layers.convolutional import Convolution1DTranspose, Conv1D
 from keras import Sequential, Model, Input
+from keras.optimizers import Adam
 from keras.layers import Dense, MaxPool1D, Flatten, Attention, BatchNormalization
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import tensorflow_compression as tfc
+import wandb
+from wandb.keras import WandbCallback
+
+from BrainwaveTools import datatypes
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+print(tf.config.list_physical_devices())
+
+wandb.init(project="Fiber-Photometry-Autoencoder", entity="bugsiesegal")
+
+def make_encoder(latent_dims):
+    return Sequential([
+        Reshape((1, 100)),
+        Conv1D(2000, 1, strides=2, use_bias=True, padding="same", activation="leaky_relu"),
+        Conv1D(5000, 1, strides=2, use_bias=True, padding="same", activation="leaky_relu"),
+        Flatten(),
+        Dense(100, use_bias=True, activation="leaky_relu"),
+        Dense(latent_dims, use_bias=True, activation=None)
+    ], name="encoder")
 
 
-def get_lr_metric(optimizer):
-    def lr(y_true, y_pred):
-        return optimizer._decayed_lr(tf.float32)  # I use ._decayed_lr method instead of .lr
-
-    return lr
-
-
-class AutoEncoderModel:
-    model: Model
-    encoder: Model
-    decoder: Model
-
-    def __init__(self, window_size, encoded_size, num_hidden_layers=2):
-        self.window_size = window_size
-        self.encoded_size = encoded_size
-        self.num_hidden_layers = num_hidden_layers
-
-    def make_model(self, config):
-        enc_in = Input((self.window_size,))
-        x = Sequential([
-            Dense(self.window_size * 8) for i in range(self.num_hidden_layers)
-        ])(enc_in)
-        enc_out = Dense(self.encoded_size, activation="sigmoid")(x)
-        dec_in = Dense(self.encoded_size)(enc_out)
-        x = Sequential([
-            Dense(self.window_size * 8) for i in range(self.num_hidden_layers)
-        ])(dec_in)
-        dec_out = Dense(self.window_size, activation="sigmoid")(x)
-
-        self.encoder = Model(inputs=enc_in, outputs=enc_out)
-        self.decoder = Model(inputs=dec_in, outputs=dec_out)
-        self.model = Model(inputs=enc_in, outputs=dec_out)
-        opt = tf.keras.optimizers.Adam(learning_rate=config["learning_rate"])
-        self.model.compile(optimizer=opt, loss="mse", metrics=['accuracy'])
-
-    def summary(self):
-        print(self.model.summary(expand_nested=False))
+def make_decoder():
+    return Sequential([
+        Dense(100, use_bias=True, activation="leaky_relu"),
+        Dense(1000, use_bias=True, activation="leaky_relu"),
+        Reshape((1, 1000)),
+        Convolution1DTranspose(2000, 1, strides=2, padding="same", activation="leaky_relu"),
+        Convolution1DTranspose(25, 1, strides=2, padding="same", activation="leaky_relu"),
+        Flatten()
+    ])
 
 
-class CNNAutoEncoderModel:
-    model: Model
-    encoder: Sequential
-    decoder: Sequential
+class FiberPhotometryAutoencoder(Model):
 
-    def __init__(self, window_size, compression_size, hidden_layer_stacks=3):
-        self.window_size = window_size
-        self.compression_size = compression_size
-        self.hidden_layer_stacks = hidden_layer_stacks
+    def __init__(self, latent_dims):
+        super().__init__()
+        self.encoder = make_encoder(latent_dims)
+        self.decoder = make_decoder()
+        self.prior_log_scales = tf.Variable(tf.zeros((latent_dims,)))
 
-    def make_model(self, config):
-        self.encoder = Sequential()
-        self.decoder = Sequential()
+    @property
+    def prior(self):
+        return tfc.NoisyLogistic(loc=0, scale=tf.exp(self.prior_log_scales))
 
-        self.encoder.add(Dense(self.window_size, input_shape=(self.window_size,)))
-        self.encoder.add(Reshape((1, self.window_size,)))
-        for i in range(int(self.hidden_layer_stacks/2)):
-            self.encoder.add(Dense(self.window_size))
-        self.encoder.add(Dense(self.window_size))
+    def call(self, x, training):
+        x = tf.cast(x, self.compute_dtype)
+        x = tf.reshape(x, (-1, 1, 100))
 
-        for i in range(self.hidden_layer_stacks):
-            self.encoder.add(Convolution1DTranspose(10, self.compression_size))
-            # self.encoder.add(BatchNormalization())
+        y = self.encoder(x)
+        entropy_model = tfc.ContinuousBatchedEntropyModel(
+            self.prior, coding_rank=1, compression=False
+        )
+        y_tilde, rate = entropy_model(y, training=training)
+        x_tilde = self.decoder(y_tilde)
 
-        for i in range(self.hidden_layer_stacks):
-            self.encoder.add(Conv1D(10, self.compression_size))
-            # self.encoder.add(BatchNormalization())
+        rate = tf.reduce_mean(rate)
 
-        self.encoder.add(Dense(self.compression_size))
-        self.encoder.add(Flatten())
-        for i in range(int(self.hidden_layer_stacks/2)):
-            self.encoder.add(Dense(self.compression_size))
+        distortion = tf.reduce_mean(abs(x - x_tilde))
 
-        self.decoder.add(Dense(self.compression_size, input_shape=(self.compression_size,)))
-        self.decoder.add(Reshape((1, self.compression_size,)))
-        for i in range(int(self.hidden_layer_stacks / 2)):
-            self.decoder.add(Dense(self.compression_size))
-
-        for i in range(self.hidden_layer_stacks):
-            self.decoder.add(Convolution1DTranspose(10, self.compression_size))
-            # self.decoder.add(BatchNormalization())
-
-        for i in range(self.hidden_layer_stacks):
-            self.decoder.add(Conv1D(10, self.compression_size))
-            # self.decoder.add(BatchNormalization())
-
-        self.decoder.add(Dense(self.window_size))
-        self.decoder.add(Flatten())
-        for i in range(int(self.hidden_layer_stacks / 2)):
-            self.decoder.add(Dense(self.window_size))
-        self.decoder.add(Dense(self.window_size, activation="sigmoid"))
+        return dict(rate=rate, distortion=distortion)
 
 
-        encoder_input = Input(shape=(self.window_size,))
-        encoder = self.encoder(encoder_input)
-        decoder = self.decoder(encoder)
-        opt = tf.keras.optimizers.Adagrad(learning_rate=config["learning_rate"], initial_accumulator_value=config["initial_accumulator_value"])
-        self.model = Model(inputs=encoder_input, outputs=decoder)
-        self.model.compile(optimizer=opt, loss="mae", metrics=['accuracy', 'mse'])
+def pass_through_loss(_, x):
+    return x
 
-    def summary(self):
-        print(self.model.summary(expand_nested=True))
+training_data = datatypes.FiberPhotometryWindowData.read("/mnt/c/Users/bugsi/PycharmProjects/brainwaveAnalysis/Test"
+                                                         "/fpw/0.fpw")
 
-class TestModel:
-    model: Model
-    encoder: Sequential
-    decoder: Sequential
-    def __init__(self, window_size, compression_size):
-        self.window_size = window_size
-        self.compression_size = compression_size
+X_train, X_test, y_train, y_test = train_test_split(training_data.data[:200], training_data.data[:200], test_size=0.33,
+                                                    random_state=42)
 
+training_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
 
-    def make_model(self, config):
-        self.encoder = Sequential()
-        self.decoder = Sequential()
+def make_compression_trainer(lmbda, latent_dims=10):
+    trainer = FiberPhotometryAutoencoder(latent_dims)
+    trainer.compile(
+        optimizer=Adam(learning_rate=1e-3),
 
-        self.encoder.add(Dense(self.window_size, input_shape=(100,), activation="sigmoid"))
-        self.encoder.add(Dense(self.window_size, activation="sigmoid"))
-        self.encoder.add(Reshape((1, self.window_size)))
-        self.encoder.add(Conv1D(self.compression_size, 1, activation="sigmoid"))
-        self.encoder.add(Flatten())
-        self.encoder.add(Dense(self.compression_size, activation="sigmoid"))
-
-        self.decoder.add(Dense(self.compression_size, input_shape=(self.compression_size,), activation="sigmoid"))
-        self.decoder.add(Reshape((1, self.compression_size)))
-        self.decoder.add(Conv1D(self.compression_size, 1, activation="sigmoid"))
-        self.decoder.add(Flatten())
-        self.decoder.add(Dense(self.window_size, activation="sigmoid"))
-        self.decoder.add(Dense(self.window_size, activation="sigmoid"))
+        loss=dict(rate=pass_through_loss, distortion=pass_through_loss),
+        metrics=dict(rate=pass_through_loss, distortion=pass_through_loss),
+        loss_weights=dict(rate=1., distortion=lmbda),
+    )
+    return trainer
 
 
-        encoder_input = Input(shape=(self.window_size,))
+def trainer_model(lmbda):
+    trainer = make_compression_trainer(lmbda)
+    trainer.fit(
+        training_dataset.batch(128).prefetch(8),
+        epochs=15,
+        validation_data=test_dataset.batch(128).cache(),
+        validation_freq=1,
+        verbose=1,
+        callbacks=[]
+    )
 
-        encoder = self.encoder(encoder_input)
+    return trainer
 
-        decoder = self.decoder(encoder)
+trainer = trainer_model(lmbda=2000)
 
-        # opt = tf.keras.optimizers.Adam(learning_rate=config["learning_rate"], beta_1=config["beta_1"], beta_2=config["beta_2"])
-        self.model = Model(inputs=encoder_input, outputs=decoder)
-        self.model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+class Encoder(Model):
+    def __init__(self, analysis_transform, entropy_model):
+        super().__init__()
+        self.entropy_model = entropy_model
+        self.analysis_transform = analysis_transform
 
-    def summary(self):
-        print(self.model.summary(expand_nested=True))
+    def call(self, x):
+        x = tf.cast(x, self.compute_dtype)
+        y = self.analysis_transform(x)
+
+        _, bits = self.entropy_model(y, training=False)
+
+        return self.entropy_model.compress(y), bits
+
+class Decoder(Model):
+    def __init__(self, synthesis_transform, entropy_model):
+        super().__init__()
+        self.entropy_model = entropy_model
+        self.synthesis_transform = synthesis_transform
+
+    def call(self, string):
+        y_hat = self.entropy_model.decompress(string, ())
+        x_hat = self.synthesis_transform(y_hat)
+
+        return x_hat
+
+def make_codec(trainer, **kwargs):
+    entropy_model = tfc.ContinuousBatchedEntropyModel(
+        trainer.prior, coding_rank=1, compression=True, **kwargs
+    )
+    compressor = Encoder(trainer.encoder, entropy_model)
+    decompressor = Decoder(trainer.decoder, entropy_model)
+
+    return compressor, decompressor
+
+compressor, decompressor = make_codec(trainer)
+
+(originals, _), = test_dataset.batch(16).skip(3).take(1)
+
+strings, entropies = compressor(originals)
+
+print(f"String representation of first digit in hexadecimal: 0x{strings[0].numpy().hex()}")
+print(f"Number of bits actually needed to represent it: {entropies[0]:0.2f}")
+
+reconstructions = decompressor(strings)
+
+fig, axs = plt.subplots(3)
+
+axs[0].plot(originals[0])
+axs[1].plot(reconstructions[0])
+
+wandb.log({"plot": fig})
+
